76d10bb1d3f74c389723788970f7deb21961fcd2
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 16:22:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -279,13 +279,14 @@ else:
     st.title("🔥 NoxVibe Feed")
     st.write("Videos publicos de la comunidad.")
 
-    c.execute("""
-            SELECT p.id, p.username, p.caption, p.file, p.fires, p.thumbs, p.hearts, p.vibe_tag, p.timestamp 
-            FROM posts p 
-            JOIN users u ON p.username = u.username 
-            WHERE p.file_type = 'video' AND u.account_privacy = 'Publico' 
-            ORDER BY p.id DESC
-        """)
+        c.execute("""
+        SELECT p.id, p.username, p.caption, p.file, p.fires, p.thumbs, p.hearts, p.vibe_tag, p.timestamp 
+        FROM posts p
+        JOIN users u ON p.username = u.username
+        WHERE p.file_type = 'video'
+        ORDER BY p.id DESC
+    """)
+      
     videos = c.fetchall()
 
     if not videos:
