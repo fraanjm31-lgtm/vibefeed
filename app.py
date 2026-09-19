641ac234b52831d8d029563f2f77cb4925bfb3b2
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 17:24:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -414,10 +414,10 @@ else:
             val_thumbs = p_thumbs if p_thumbs is not None else 0
             val_hearts = p_hearts if p_hearts is not None else 0
             
-                
-               col_vid, col_act = st.columns((4, 1))
-              
-           with col_act:            
+            col_vid, col_act = st.columns((4, 1)) 
+               
+               
+             with col_act:            
              st.markdown("<br><br>", unsafe_allow_html=True)
                if st.button(
                f"🔥 {val_fires}", key=f"feed_fire_{p_id}", use_container_width=True
