e17522b92fa39a93e97b78ba1bef9154737feba0
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 12:39:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -610,8 +610,7 @@ else:
          st.warning("Usuario no encontrado.")
 
 if menu_option == "⚙️ Ajustes":
-    st.title("⚙️ Ajustes de la cuenta")
-        
+    st.title("⚙️ Ajustes de la cuenta")   
         c.execute(
         "SELECT theme, account_privacy FROM users WHERE username = ?", (cur,)
     )
