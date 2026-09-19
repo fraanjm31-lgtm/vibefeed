21e963bdaf4b8985d235612ef188d90e7c88faf0
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-20 01:18:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -386,10 +386,14 @@ elif menu_option == "👤 Mi Perfil":
     c.execute("SELECT id, username, caption, file, fires, thumbs, hearts, vibe_tag, timestamp FROM posts WHERE username = ? ORDER BY id DESC", (cur,))
     videos_usuario = c.fetchall()
 
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Posts", len(videos_usuario))
-    col2.metric("Seguidores", 0)
-    col3.metric("Siguiendo", 0)
+        col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric("Posts", len(videos_usuario))
+    with col2:
+        st.metric("Seguidores", 0)
+    with col3:
+        st.metric("Siguiendo", 0)
+        
 
 
 
