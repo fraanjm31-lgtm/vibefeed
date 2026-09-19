5753d074e1b47f1b8fbccd9f70ddcad1270361cd
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 02:55:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -671,10 +671,12 @@ else:
         
 def enviar_codigo_correo(destinatario, codigo):
     st.session_state["codigo_generado"] = codigo
-    st.warning(f"🔑 Tu código de verificación temporal es: **{codigo}**")
+
     return True
     
+
 if st.button("Obtener Código"):
     codigo_aleatorio = str(random.randint(1000, 9999))
     enviar_codigo_correo("", codigo_aleatorio)
+    st.warning(f"🔑 Tu código de verificación temporal es: **{codigo_aleatorio}**")
     
