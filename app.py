fc1ab7222a0da260a9b227c150da43b20068e18d
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 23:20:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -461,8 +461,7 @@ if menu_option == "👤 Mi Perfil":
                 if p_file and isinstance(p_file, str) and os.path.exists(p_file):
                     st.video(p_file)
                 st.markdown("---")
-    else:
-        st.warning("Usuario no encontrado.")
+    
         
         
         st.markdown("### 🎬 Tus Videos")
