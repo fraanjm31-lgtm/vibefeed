4c938a36d109becfcd2cfb727c4e3cd4f003b61b
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-19 22:33:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -614,8 +614,8 @@ else:
         st.warning("Usuario no encontrado.")
 
       elif menu_option == "👥 Siguiendo":
-    st.title("👥 Siguiendo")
-    st.write("Videos recientes de la gente a la que sigues.")
+        st.title("👥 Siguiendo")
+        st.write("Videos recientes de la gente a la que sigues.")
     
     c.execute("SELECT followed FROM follows WHERE follower = ? AND status = 'accepted'", (cur,))
     siguiendo = [r[0] for r in c.fetchall()]
