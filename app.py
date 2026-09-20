5c5089e14637a731709be72988d781b1585e9e11
fraanjm31-lgtm <fraanjm31@gmail.com>
2026-09-20 16:28:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ st.markdown(
     }
     .bottom-nav a {
         color: rgb(255,255,255);
-        font-size: 20px;
+        c += "font-size: 20px;"
+
     }
     </style>
 """, unsafe_allow_html=True)
